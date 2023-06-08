468695f3fc0eed125b104f997ac345aa107e50bb
Anik Barua <anikbb04@gmail.com>
2023-06-08 18:54:56 +0600

    make admin and instructor functionility implemented

--- src/Components/Dashboard/AdminDashBoard/ManageUsers/ManageUsers.jsx
+++ src/Components/Dashboard/AdminDashBoard/ManageUsers/ManageUsers.jsx
@@ -3,6 +3,7 @@ import React from 'react';
 import { HiOutlineTrash } from 'react-icons/hi';
 import { MdNavigateNext } from 'react-icons/md';
 import { Link } from 'react-router-dom';
+import Swal from 'sweetalert2';
 
 const ManageUsers = () => {
 
@@ -13,6 +14,44 @@ const ManageUsers = () => {
             .then(res => res.json())
     })
     // console.log(students)
+    const handleMakeInstructor = (id) => {
+        fetch(`http://localhost:5000/users/${id}`, {
+            method: 'PATCH',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: `${user?.name} is promoted as Instructor`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    }
+    const handleMakeAdmin = (id) => {
+        fetch(`http://localhost:5000/admin/users/${id}`, {
+            method: 'PATCH',
+        })
+            .then(res => res.json())
+            .then(data => {
+                console.log(data)
+                if (data.modifiedCount) {
+                    refetch();
+                    Swal.fire({
+                        position: 'center',
+                        icon: 'success',
+                        title: `${user?.name} is promoted as Admin`,
+                        showConfirmButton: false,
+                        timer: 1500
+                    })
+                }
+            })
+    }
 
 
     return (
@@ -57,14 +96,14 @@ const ManageUsers = () => {
                                 <td className=' font-playfair text-base font-normal'>{student?.email}</td>
                                 <td className=''>
                                     <button
-                                        // onClick={() => handlePayFees(course._id)}
+                                        onClick={() => handleMakeInstructor(student?._id)}
                                         className="bg-orange-100 w-full max-w-xs h-8 font-playfair text-base rounded-md">
                                         Make Instructor
                                     </button>
                                 </td>
                                 <td className=''>
                                     <button
-                                        // onClick={() => handleRemoveClass(course._id)}
+                                        onClick={() => handleMakeAdmin(course._id)}
                                         className="bg-orange-100 w-full max-w-xs h-8 font-playfair text-base rounded-md">
                                         Make Admin
                                     </button>
