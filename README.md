79c41758d1931928b76c94de325aa02c0d74e39b
Anik Barua <anikbb04@gmail.com>
2023-06-14 14:35:36 +0600

    mention all the website featue in readme file

---
+++ README.md
@@ -0,0 +1,24 @@
+# Crown Drawing School Website
+
+# Feature
+
+* User can buy differant courses easily
+* User can see his/her booking class,enrolled class,payment history
+* Instructor/course teacher Can Add a class,see course status
+* Admin Can approve,denied pending class,make user admin,make user instructor,give course feedback
+* User can see popular instructor based on enrolled students
+
+# Css framework
+
+* Tailwind css
+
+# Packages
+* Firebase
+* Axios
+* Transtack query
+* Daisy ui
+* Reack Hook Form
+* React Reveal
+
+# Live Site Link
+open 
--- src/Components/Pages/Home/PopularInstructorSection/PopularInstructorSection.jsx
+++ src/Components/Pages/Home/PopularInstructorSection/PopularInstructorSection.jsx
@@ -1,19 +1,27 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 
 const PopularInstructorSection = () => {
-    const classes = [
-        { className: 'Still Life', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 30 },
-        { className: 'Potraits', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 25 },
-        { className: 'LandScapes', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 40 },
-        { className: 'Animals', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 20 },
-        { className: 'Architecture', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 35 },
-        { className: 'AbstractArt', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 15 },
-        { className: 'Nature&Botanicals', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 50 },
-        { className: 'Figure Drawing', numOfStudents: 10 },
-    ];
-
-    classes.sort((a, b) => b.numOfStudents - a.numOfStudents);
-    const topClasses = classes.slice(0, 6);
+    // const classes = [
+    //     { className: 'Still Life', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 30 },
+    //     { className: 'Potraits', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 25 },
+    //     { className: 'LandScapes', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 40 },
+    //     { className: 'Animals', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 20 },
+    //     { className: 'Architecture', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 35 },
+    //     { className: 'AbstractArt', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 15 },
+    //     { className: 'Nature&Botanicals', image: 'https://i.ibb.co/cQGgyv0/pexels-pavel-danilyuk-8382370-1536x1025-1.png', numOfStudents: 50 },
+    //     { className: 'Figure Drawing', numOfStudents: 10 },
+    // ];
+    const { isLoading, refetch, data: instructors } = useQuery({
+        queryKey: ['popular'],
+        queryFn: () => fetch(`http://localhost:5000/instructors/popular`)
+            .then(res => res.json())
+    })
+    // console.log(instructors)
+    const enrolledStudent = instructors?.filter((student) => student.enrolled)
+    // console.log(enrolledStudent)
+    enrolledStudent?.sort((a, b) => b.enrolled - a.enrolled);
+    const topClasses = enrolledStudent?.slice(0, 6);
     return (
         <div className='w-full max-w-7xl mx-auto mt-20'>
             <h1 className='font-playfair font-extrabold text-4xl text-center mb-5 hover:text-orange-100'>Popular Instructors</h1>
@@ -24,9 +32,9 @@ const PopularInstructorSection = () => {
                         className='w-full max-w-[356px] h-[250px] mx-auto text-center'
                     >
 
-                        <img className='w-full max-w-[300px] h-48 mx-auto mb-5' src={classItem?.image} alt="classname image" />
+                        <img className='w-full max-w-[300px] h-48 mx-auto mb-5' src={classItem?.photo} alt="classname image" />
                         {/* <h3 className='font-playfair font-bold text-2xl text-zinc-100'>{classItem?.className}</h3> */}
-                        <p className='font-playfair font-semibold text-xl text-zinc-100'>Students:{classItem?.numOfStudents}</p>
+                        <p className='font-playfair font-semibold text-xl text-zinc-100'>Students:{classItem?.enrolled}</p>
 
                     </div>
                     )
