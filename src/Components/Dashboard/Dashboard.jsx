c8a17bee258bd6a6f4f3e01bb947c776ca6facf8
Anik Barua <anikbb04@gmail.com>
2023-06-14 15:58:31 +0600

    make not found page

--- src/Components/Dashboard/Dashboard.jsx
+++ src/Components/Dashboard/Dashboard.jsx
@@ -13,109 +13,115 @@ const Dashboard = () => {
     const [isInstructor] = UseInstructor()
 
     return (
-        <div>
-            <div className="drawer lg:drawer-open">
-                <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
-                <div className="drawer-content flex flex-col mx-10">
-                    {/* Page content here */}
-                    <Outlet></Outlet>
-                    <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+        <>
+            <div>
+                <div className="drawer lg:drawer-open">
 
-                </div>
-                <div className="drawer-side">
-                    <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
-                    <ul className="menu p-4 w-80 h-full bg-zinc-100">
-                        {/* Sidebar content here */}
+                    <input id="my-drawer-2" type="checkbox" className="drawer-toggle" />
+                    <div className="drawer-content flex flex-col mx-10">
+                        {/* Page content here */}
+                        <label htmlFor="my-drawer-2" className="bg-orange-100 w-full max-w-[160px] h-11 flex items-center justify-center text-center rounded-md lg:hidden">Open drawer</label>
+                        <div className=''>
+                            <Outlet></Outlet>
+                        </div>
+                        {/* <label htmlFor="my-drawer-2" className="btn btn-primary drawer-button lg:hidden">Open drawer</label> */}
 
-                        {
-                            isInstructor?.instructor &&
-                            <>
-                                {/* instructor dashboard routes starts here */}
-                                <li>
-                                    <NavLink to={'instructor/add'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
-                                        <MdLibraryAdd className='mr-3 text-2xl'></MdLibraryAdd>
-                                        Add A Class
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'instructor/classes'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
-                                        <SiGoogleclassroom className='mr-3 text-2xl'></SiGoogleclassroom>
-                                        My Classes
-                                    </NavLink>
-                                </li>
-                                {/* instructor dashboard routes ends here  */}
-                            </>
+                    </div>
+                    <div className="drawer-side">
+                        <label htmlFor="my-drawer-2" className="drawer-overlay"></label>
+                        <ul className="menu p-4 w-80 h-full bg-zinc-100">
+                            {/* Sidebar content here */}
 
-                        }
+                            {
+                                isInstructor?.instructor &&
+                                <>
+                                    {/* instructor dashboard routes starts here */}
+                                    <li>
+                                        <NavLink to={'instructor/add'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
+                                            <MdLibraryAdd className='mr-3 text-2xl'></MdLibraryAdd>
+                                            Add A Class
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to={'instructor/classes'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
+                                            <SiGoogleclassroom className='mr-3 text-2xl'></SiGoogleclassroom>
+                                            My Classes
+                                        </NavLink>
+                                    </li>
+                                    {/* instructor dashboard routes ends here  */}
+                                </>
 
-                        {
-                            isAdmin?.admin &&
-                            <>
-                                {/* admin dashboard routes starts here */}
-                                <li>
-                                    <NavLink to={'admin/classes/manage'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
-                                        <SiGoogleclassroom className='mr-3 text-2xl'></SiGoogleclassroom>
-                                        Manage Classes
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'admin/classes/users'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
-                                        <MdOutlineManageAccounts className='mr-3 text-2xl'></MdOutlineManageAccounts>
-                                        Manage Users
-                                    </NavLink>
-                                </li>
-                                {/* admin dashboard routes ends here  */}
-                            </>
+                            }
 
-                        }
-                        {isAdmin?.admin || isInstructor?.instructor ||
-                            <>
-                                {/* student dashboard routes */}
-                                <li>
-                                    <NavLink to={'classes/selected'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        <MdAdsClick className='text-2xl mr-3'></MdAdsClick>
-                                        My Selected Classes
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'classes/enrolled'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        <TfiWrite className='text-2xl mr-3'></TfiWrite>
-                                        My Enrolled Classes
-                                    </NavLink>
-                                </li>
-                                <li>
-                                    <NavLink to={'payments'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                        {/* <TfiWrite className='text-2xl mr-3'></TfiWrite> */}
-                                        <MdPayment className='text-2xl mr-3'></MdPayment>
-                                        My Payment History
-                                    </NavLink>
-                                </li>
-                                {/* <li>
+                            {
+                                isAdmin?.admin &&
+                                <>
+                                    {/* admin dashboard routes starts here */}
+                                    <li>
+                                        <NavLink to={'admin/classes/manage'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
+                                            <SiGoogleclassroom className='mr-3 text-2xl'></SiGoogleclassroom>
+                                            Manage Classes
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to={'admin/classes/users'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            {/* <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle> */}
+                                            <MdOutlineManageAccounts className='mr-3 text-2xl'></MdOutlineManageAccounts>
+                                            Manage Users
+                                        </NavLink>
+                                    </li>
+                                    {/* admin dashboard routes ends here  */}
+                                </>
+
+                            }
+                            {isAdmin?.admin || isInstructor?.instructor ||
+                                <>
+                                    {/* student dashboard routes */}
+                                    <li>
+                                        <NavLink to={'classes/selected'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            <MdAdsClick className='text-2xl mr-3'></MdAdsClick>
+                                            My Selected Classes
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to={'classes/enrolled'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            <TfiWrite className='text-2xl mr-3'></TfiWrite>
+                                            My Enrolled Classes
+                                        </NavLink>
+                                    </li>
+                                    <li>
+                                        <NavLink to={'payments'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                            {/* <TfiWrite className='text-2xl mr-3'></TfiWrite> */}
+                                            <MdPayment className='text-2xl mr-3'></MdPayment>
+                                            My Payment History
+                                        </NavLink>
+                                    </li>
+                                    {/* <li>
                                     <NavLink to={'classes/payments'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
                                         <AiOutlinePayCircle className='mr-3 text-2xl'></AiOutlinePayCircle>
                                         My Payment Page
                                     </NavLink>
                                 </li> */}
-                                {/* student dashboard routes ends here  */}
-                            </>
-                        }
+                                    {/* student dashboard routes ends here  */}
+                                </>
+                            }
 
-                        <li>
-                            <NavLink to={'/'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
-                                <AiOutlineHome className='text-2xl mr-3'></AiOutlineHome>
-                                Home
-                            </NavLink>
-                        </li>
+                            <li>
+                                <NavLink to={'/'} className={({ isActive }) => isActive ? 'dashboard-true' : 'dashboard-false'}>
+                                    <AiOutlineHome className='text-2xl mr-3'></AiOutlineHome>
+                                    Home
+                                </NavLink>
+                            </li>
 
-                    </ul>
+                        </ul>
 
+                    </div>
                 </div>
             </div>
-        </div>
+        </>
     );
 };
 
--- src/Components/Shared/NotFound/NotFound.jsx
+++ src/Components/Shared/NotFound/NotFound.jsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import Lottie from 'lottie-react';
+import notfound from './drawing.json'
+import { useNavigate } from 'react-router-dom';
 
 const NotFound = () => {
+    const navigate = useNavigate();
     return (
-        <div>
-
-        </div>
+        <div className='relative'>
+            <button className='absolute z-10 bg-orange-100 shadow-md w-40 h-10 rounded-lg font-roboto font-bold text-xl border border-gray-100'
+                onClick={() => navigate('/')}>Back To Home</button>
+            <Lottie
+                // classNameh={'h-40'}
+                style={{ height: '400px' }}
+                animationData={notfound}
+                loop={true}
+            ></Lottie>
+        </div >
     );
 };
 
