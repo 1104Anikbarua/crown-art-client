2c46e4de3d1fc59e0768b6f2295ee2a56ad81cb3
Anik Barua <anikbb04@gmail.com>
2023-06-14 11:57:33 +0600

    use animation in instructor card

--- src/Components/Pages/Home/Instructors/Instructors.jsx
+++ src/Components/Pages/Home/Instructors/Instructors.jsx
@@ -1,9 +1,13 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
 import { MdNavigateNext } from 'react-icons/md'
 import { useQuery } from '@tanstack/react-query';
 import LoadingSpinner from '../../../Shared/LoadingSpinner/LoadingSpinner';
+import { DrawingContext } from '../../../AuthProvider/AuthProvider';
+import Swal from 'sweetalert2';
+import { Fade, Flip, Zoom } from 'react-reveal';
 const Instructors = () => {
+    const { user } = useContext(DrawingContext)
     // const classes = [
     //     { className: 'Still Life', teacherName: 'Mr x', image: 'https://i.ibb.co/QK9VMy8/still-life.png', numOfStudents: 30 },
     //     { className: 'Potraits', teacherName: 'Mr x', image: 'https://i.ibb.co/Pr4gV64/potrait.png', numOfStudents: 25 },
@@ -16,10 +20,10 @@ const Instructors = () => {
     // ];
     const { isLoading, refetch, data: instructors } = useQuery({
         queryKey: ['instructors'],
-        queryFn: () => fetch(`http://localhost:5000/classes`)
+        queryFn: () => fetch(`http://localhost:5000/instructors`)
             .then(res => res.json())
     })
-    console.log(isLoading, instructors)
+    // console.log(isLoading, instructors)
     const uniqueEmailsSet = new Set();
 
     const uniqueEmailArray = instructors?.filter((obj) => {
@@ -32,6 +36,32 @@ const Instructors = () => {
         return false;
     });
     // console.log(uniqueEmailArray)
+    const uniqueInsturctor = uniqueEmailArray?.filter((user) => user?.role === 'instructor')
+    // console.log(uniqueInsturctor)
+    // const handleInstructor = (id) => {
+    //     const userInfo = { photo: user?.photoURL }
+    //     fetch(`http://localhost:5000/instructor/update/${id}`, {
+    //         method: 'PATCH',
+    //         headers: {
+    //             'content-type': 'application/json'
+    //         },
+    //         body: JSON.stringify(userInfo)
+    //     })
+    //         .then(res => res.json())
+    //         .then(data => {
+    //             // console.log(data)
+    //             if (data.modifiedCount) {
+    //                 refetch();
+    //                 Swal.fire({
+    //                     position: 'center',
+    //                     icon: 'success',
+    //                     title: `User is promoted as Instructor`,
+    //                     showConfirmButton: false,
+    //                     timer: 1500
+    //                 })
+    //             }
+    //         })
+    // }
     return (
         <>
             {
@@ -49,21 +79,32 @@ const Instructors = () => {
                             <MdNavigateNext className='mx-5'></MdNavigateNext>
                             <p className='text-black font-montserrat text-base font-medium'>Instructors</p>
                         </div>
+                        {/* <Zoom duration={3000} delay={1000}> */}
                         <div className='w-full max-w-7xl mx-auto grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-5'>
                             {
-                                uniqueEmailArray?.map((teacher) => <div
-                                    key={teacher._id}
-                                    className='w-full max-w-[356px] h-[459px]'
-                                >
+                                uniqueInsturctor?.map((teacher, index) => <Fade
+                                    duration={index * 100}
+                                    delay={index * 2000}>
+                                    <div
+                                        key={teacher._id}
+                                        className='w-full max-w-[356px] h-[330px] mx-auto bg-orange-100 py-6'
+                                    >
 
-                                    <img className='w-full max-w-[300px] h-48 mb-5' src={teacher?.photo} alt="classname image" />
-                                    <h3 className='font-playfair font-bold text-2xl text-zinc-100'>{teacher?.instructorName}</h3>
-                                    <p className='font-playfair font-semibold text-xl text-zinc-100'>Email:{teacher?.email}</p>
+                                        <img className='w-full max-w-[300px] h-48 mb-5 mx-auto' src={teacher?.image} alt="classname image" />
+                                        <div className='ml-6'>
+                                            <h3 className='font-playfair font-bold text-2xl text-zinc-100'>Name:{teacher?.name}</h3>
+                                            <p className='font-playfair font-semibold text-xl text-zinc-100'>Email:{teacher?.email}</p>
+                                        </div>
+                                        {/* <button
+                                        onClick={() => handleInstructor(teacher?._id)}
+                                        className='bg-orange-100'>Update</button> */}
 
-                                </div>)
+                                    </div>
+                                </Fade>)
                             }
                         </div>
-                    </div>
+                        {/* </Zoom> */}
+                    </div >
             }
         </>
     );
